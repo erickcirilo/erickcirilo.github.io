d5ea131bc35910b6c0dc117ddfd4d4d681ee034d
WDD 230 Student Account <erickcirilo@byui.edu>
2021-04-22 22:39:03 -0300

    Javascript date updated

--- javascript/update.js
+++ javascript/update.js
@@ -1,6 +1,2 @@
-//current date on home page
-let time = new Date();
-document.querySelector('#year').textContent = time.getFullYear();
 
-//last update on home page
-document.querySelector('#latestEdit').textContent = new Date(document.lastModified);
+document.querySelector('#latestEdit').textContent = document.lastModified;
