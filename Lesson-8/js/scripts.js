077e1479828b2796f32fa48bc2b5028e819cf3e7
Erick Cirilo <ciriloerick@gmail.com>
2021-06-14 00:23:33 -0300

    complete assignment week 8

---
+++ Lesson-8/js/scripts.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
+function adjustRating(rating) {
+  document.getElementById("ratingvalue").innerHTML = rating;
+}
